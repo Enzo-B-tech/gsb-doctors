fa496a90a72609a22c59a667516282cb52562bec
Bouam Enzo <enzo_bouam@yahoo.com>
2025-11-18 11:11:06 +0100

    feat(pages): add DoctorsPageComponent with signal

---
+++ src/app/pages/doctors-page/doctors-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'doctors-page',
+  imports: [],
+  templateUrl: './doctors-page.html',
+  styleUrl: './doctors-page.css',
+})
+export class DoctorsPage {
+
+}
